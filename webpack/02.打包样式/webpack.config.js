3d8678fa824b85069cf0506ec745b8990f50318e
廖启准 <1141446749@qq.com>
2020-07-22 14:22:28 +0800

    css打包

--- webpack/02.打包样式/webpack.config.js
+++ webpack/02.打包样式/webpack.config.js
@@ -4,6 +4,8 @@
    所有构建工具都是基于nodejs平台运行的，模块化默认commonjs。
  */
 
+ const {resolve} = require('path')
+
  module.exports = {
      // 入口起点
     entry:'./src/index.js',
@@ -19,6 +21,28 @@
     module:{
         rules:[
             // 详细loader配置
+            //不同文件必须配置不同loader处理
+            {
+                //匹配哪些文件
+                test:/\.css$/,
+                // 使用哪些loader
+                use:[
+                    // use数组中loader执行顺序，从右往左，从下往上
+                    //创建style标签，将js中的样式资源插入进行，添加到head中生效
+                    'style-loader',
+                    //将css文件变成commonjs模块加载js中，里面内容是样式字符串
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.less$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    //将less编译成css文件
+                    'less-loader'
+                ]
+            }
         ]
     },
     plugins:[
